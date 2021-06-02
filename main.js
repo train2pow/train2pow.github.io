2fdd92899a89c5d18e34df1a6cf69b4fcb9ffb73
Anna Siebenbrunner <72545186+anna7br@users.noreply.github.com>
2021-06-02 08:27:51 +0200

    baselayers und Kartenmittelpunkt definieren

--- main.js
+++ main.js
@@ -1,2 +1,24 @@
 // Kartenscript
 
+// Kartenhintergründe der basemap.at definieren
+let baselayers = {
+    standard: L.tileLayer.provider("BasemapAT.basemap"),
+    grau: L.tileLayer.provider("BasemapAT.grau"),
+    terrain: L.tileLayer.provider("BasemapAT.terrain"),
+    surface: L.tileLayer.provider("BasemapAT.surface"),
+    highdpi: L.tileLayer.provider("BasemapAT.highdpi"),
+    ortho_overlay: L.layerGroup([
+        L.tileLayer.provider("BasemapAT.orthofoto"),
+        L.tileLayer.provider("BasemapAT.overlay")
+    ]),
+};
+
+// Karte initialisieren und auf Österreichs Mittelpunkt blicken
+let map = L.map("map", {
+    fullscreenControl: true,
+    center: [47.71216, 13.34290],
+    zoom: 6.5,
+    layers: [
+        baselayers.grau
+    ]
+});
