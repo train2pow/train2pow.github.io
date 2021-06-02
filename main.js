bceca9c78098f6dd17cb63eabd9976f5174617e0
Anna Siebenbrunner <72545186+anna7br@users.noreply.github.com>
2021-06-02 10:42:27 +0200

    customize icon

--- main.js
+++ main.js
@@ -10,6 +10,15 @@ let map = L.map("map", {
     ]
 });
 
+
+// create custom snowflake icon
+var snowflake = L.icon({
+    iconUrl: 'snowflake.png',
+    iconSize: [20, 20], // size of the icon
+    iconAnchor: [10, 10], // point of the icon which will correspond to marker's location
+    popupAnchor: [0, 0] // point from which the popup should open relative to the iconAnchor
+});
+
 // add POW Watermark
 L.Control.Watermark = L.Control.extend({
     onAdd: function (map) {
