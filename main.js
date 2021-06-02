95216a452958ab97f35295d38d5530ed7daf784d
Anna Siebenbrunner <72545186+anna7br@users.noreply.github.com>
2021-06-02 08:44:33 +0200

    fix bugs and switch to OSM as background map

--- main.js
+++ main.js
@@ -1,48 +1,24 @@
 // Kartenscript
 
-// Kartenhintergründe der basemap.at definieren
-let baselayers = {
-    standard: L.tileLayer.provider("BasemapAT.basemap"),
-    grau: L.tileLayer.provider("BasemapAT.grau"),
-    terrain: L.tileLayer.provider("BasemapAT.terrain"),
-    surface: L.tileLayer.provider("BasemapAT.surface"),
-    highdpi: L.tileLayer.provider("BasemapAT.highdpi"),
-    ortho_overlay: L.layerGroup([
-        L.tileLayer.provider("BasemapAT.orthofoto"),
-        L.tileLayer.provider("BasemapAT.overlay")
-    ]),
-};
-
 // Karte initialisieren und auf Österreichs Mittelpunkt blicken
 let map = L.map("map", {
     fullscreenControl: true,
     center: [47.71216, 13.34290],
     zoom: 6.5,
     layers: [
-        baselayers.grau
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')
     ]
 });
 
-// Kartenhintergründe und Overlays zur Layer-Control hinzufügen
-let layerControl = L.control.layers({
-    "basemap.at Standard": baselayers.standard,
-    "basemap.at grau": baselayers.grau,
-    "basemap.at Relief": baselayers.terrain,
-    "basemap.at Oberfläche": baselayers.surface,
-    "basemap.at hochauflösend": baselayers.highdpi,
-    "basemap.at Orthofoto beschriftet": baselayers.ortho_overlay
-}).addTo(map);
-
-
 // add POW Watermark
 L.Control.Watermark = L.Control.extend({
-    onAdd: function (mymap) {
+    onAdd: function (map) {
         var img = L.DomUtil.create('img');
         img.src = '_POW_AT_lang_blue.png';
         img.style.width = '100px';
         return img;
     },
-    onRemove: function (mymap) {
+    onRemove: function (map) {
         // Nothing to do here
     }
 });
@@ -51,4 +27,4 @@ L.control.watermark = function (opts) {
 }
 L.control.watermark({
     position: 'topright'
-}).addTo(mymap);
+}).addTo(map);
